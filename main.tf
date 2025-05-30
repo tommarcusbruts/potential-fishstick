f09839c03820f580fb79ab978a7ee55966cdd80b
tommarcusbruts <tommarcusbrut@gmail.com>
2025-05-30 12:28:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_container"
+  image = docker_image.nginx.name  # Updated to avoid deprecated 'latest'
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
